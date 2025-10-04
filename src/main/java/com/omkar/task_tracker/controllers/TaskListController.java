57ae458620592175b9bc0a5198199e6528b8a451
Omkar Padalkar <82218332+omkarpadalkar21@users.noreply.github.com>
2025-10-04 22:13:08 +0530

    Update DB and create controllers and services

---
+++ src/main/java/com/omkar/task_tracker/controllers/TaskListController.java
@@ -0,0 +1,30 @@
+package com.omkar.task_tracker.controllers;
+
+import com.omkar.task_tracker.domain.dto.TaskListDto;
+import com.omkar.task_tracker.mappers.TaskListMapper;
+import com.omkar.task_tracker.services.TaskListService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/task-lists")
+public class TaskListController {
+
+    private final TaskListService taskListService;
+    private final TaskListMapper taskListMapper;
+
+    public TaskListController(TaskListService taskListService, TaskListMapper taskListMapper) {
+        this.taskListService = taskListService;
+        this.taskListMapper = taskListMapper;
+    }
+
+    @GetMapping
+    public List<TaskListDto> listTaskLists() {
+        return taskListService.listTaskLists().stream()
+                .map(taskListMapper::toDto)
+                .toList();
+    }
+}
