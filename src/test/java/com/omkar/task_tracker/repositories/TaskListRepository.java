8163d86f9fc2ad8dfd53f8000c297dc91dba1513
Omkar Padalkar <82218332+omkarpadalkar21@users.noreply.github.com>
2025-10-04 18:45:56 +0530

    Add Task and TaskList repositories

---
+++ src/test/java/com/omkar/task_tracker/repositories/TaskListRepository.java
@@ -0,0 +1,11 @@
+package com.omkar.task_tracker.repositories;
+
+import com.omkar.task_tracker.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+}
