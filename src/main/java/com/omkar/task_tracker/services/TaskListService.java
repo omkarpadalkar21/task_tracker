4ae6ed6bb18108d369763a46853f784aaba54080
Omkar Padalkar <82218332+omkarpadalkar21@users.noreply.github.com>
2025-10-05 22:21:07 +0530

    Add method to get task list

--- src/main/java/com/omkar/task_tracker/services/TaskListService.java
+++ src/main/java/com/omkar/task_tracker/services/TaskListService.java
@@ -3,8 +3,13 @@ package com.omkar.task_tracker.services;
 import com.omkar.task_tracker.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
+
     TaskList createTaskList(TaskList taskList);
+
+    Optional<TaskList> getTaskList(UUID id);
 }
