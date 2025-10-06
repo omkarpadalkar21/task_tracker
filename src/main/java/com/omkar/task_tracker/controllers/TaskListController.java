c1411bf9482f07eecd00eaff60776907bf9b0cc0
Omkar Padalkar <82218332+omkarpadalkar21@users.noreply.github.com>
2025-10-06 17:20:06 +0530

    Add method to update task list

--- src/main/java/com/omkar/task_tracker/controllers/TaskListController.java
+++ src/main/java/com/omkar/task_tracker/controllers/TaskListController.java
@@ -24,23 +24,25 @@ public class TaskListController {
 
     @GetMapping
     public List<TaskListDto> listTaskLists() {
-        return taskListService.listTaskLists()
-                .stream()
-                .map(taskListMapper::toDto)
-                .toList();
+        return taskListService.listTaskLists().stream().map(taskListMapper::toDto).toList();
     }
 
     @PostMapping
     public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto) {
-        TaskList createdTaskList = taskListService.createTaskList(
-                taskListMapper.fromDto(taskListDto)
-        );
+        TaskList createdTaskList = taskListService.createTaskList(taskListMapper.fromDto(taskListDto));
         return taskListMapper.toDto(createdTaskList);
     }
 
     @GetMapping(path = "/{task_list_id}")
-    public Optional<TaskListDto> getTaskList(@PathVariable("task_list_id") UUID taskListId){
+    public Optional<TaskListDto> getTaskList(@PathVariable("task_list_id") UUID taskListId) {
         return taskListService.getTaskList(taskListId).map(taskListMapper::toDto);
+    }
+
+    @PutMapping(path = "/{task_list_id}")
+    public TaskListDto updateTaskList(@PathVariable("task_list_id") UUID taskListId, @RequestBody TaskListDto taskListDto) {
+        TaskList updatedTaskList = taskListService.updateTaskList(taskListId, taskListMapper.fromDto(taskListDto));
+
+        return taskListMapper.toDto(updatedTaskList);
     }
 
 }
--- src/main/java/com/omkar/task_tracker/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/omkar/task_tracker/services/impl/TaskListServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,10 +27,10 @@ public class TaskListServiceImpl implements TaskListService {
 
     @Override
     public TaskList createTaskList(TaskList taskList) {
-        if(null != taskList.getId()) {
+        if (null != taskList.getId()) {
             throw new IllegalArgumentException("Task list already has an ID!");
         }
-        if(null == taskList.getTitle() || taskList.getTitle().isBlank()) {
+        if (null == taskList.getTitle() || taskList.getTitle().isBlank()) {
             throw new IllegalArgumentException("Task list title must be present!");
         }
 
@@ -48,5 +49,25 @@ public class TaskListServiceImpl implements TaskListService {
     public Optional<TaskList> getTaskList(UUID id) {
         return taskListRepository.findById(id);
     }
+
+    @Override
+    public TaskList updateTaskList(UUID taskListId, TaskList taskList) {
+        if (null == taskList.getId()) {
+            throw new IllegalArgumentException("Task list must have an ID");
+        }
+
+        if (!Objects.equals(taskList.getId(), taskListId)) {
+            throw new IllegalArgumentException("Attempting to change task list ID, this is not permitted!");
+        }
+
+        TaskList existingTaskList = taskListRepository.findById(taskListId).orElseThrow(() ->
+                new IllegalArgumentException("Task list not found!"));
+
+        existingTaskList.setTitle(taskList.getTitle());
+        existingTaskList.setDescription(taskList.getDescription());
+        existingTaskList.setUpdated(LocalDateTime.now());
+        return taskListRepository.save(existingTaskList);
+    }
+
 
 }
